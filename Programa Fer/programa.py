aba11fc5c4c253065a94b5386fe23f94bf32f6c3
LostRot <89678254+LostRot@users.noreply.github.com>
2022-03-14 23:45:58 -0600

    Primera documentación

--- Programa Fer/programa.py
+++ Programa Fer/programa.py
@@ -1,10 +1,13 @@
 import funciones as f
 import math as m
 print("*************************************************")
+
+# Definicion de variables e inicializacion de estas por medio de funciones
 longitud=f.validarReal("Ingrese la longitud: ")
 diametro=f.validarReal("Ingrese el diametro: ")
 altura=f.validarReal("Ingrese la altura: ")
 
+#Llamado de la funcion que calcula el volumen del liquido
 f.volumenLiquido(altura,diametro,longitud)
 #numero= input("Ingrese un numero:  ")
 '''
